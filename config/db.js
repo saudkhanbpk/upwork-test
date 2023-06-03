17d9bbf2eb200e8e22193278c7845f6cc65683e0
Saud Khan <99093073+saudkhanbpk@users.noreply.github.com>
2023-06-03 11:29:44 +0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 const database = async () => {
   const db = mongoose.connection;
   mongoose.set('strictQuery', false)
-  mongoose.connect(process.env.DATABASE_URL)
+  mongoose.connect(process.env.DATABASE_URL ? process.env.DATABASE_URL : "mongodb+srv://Roadmap-Software:393939Sk@cluster0.syewhvy.mongodb.net/coregenion")
   db.on('error', console.error.bind(console, 'mongodb connection error'));
   db.once('open', function () {
     console.log('Database connected');
